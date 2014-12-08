b2588a32dd8d9497ce8d9449107b1eb603bf94e4
Simon Kim <simon.ty.kim@gmail.com>
2014-12-08 02:39:31 -0800

    ohemgee services done at 3... am.

--- app/js/client.js
+++ app/js/client.js
@@ -1,7 +1,20 @@
 'use strict';
 
 require('angular/angular');
+require('angular-route');
 
-var mmmApp = angular.module('mmmApp', []);
+var mmmApp = angular.module('mmmApp', ['ngRoute']);
 
+require('./services/mmm_service')(mmmApp);
 require('./controllers/mmm_controller')(mmmApp);
+
+mmmApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/mmm', {
+    templateUrl: 'templates/mmm/mmm_template.html',
+    controller: 'mmmCtrl'
+  })
+  .otherwise({
+    redirectTo: '/mmm'
+  });
+}]);
--- app/js/controllers/mmm_controller.js
+++ app/js/controllers/mmm_controller.js
@@ -1,27 +1,19 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('mmmCtrl', ['$scope', '$http', function($scope, $http) {
+  app.controller('mmmCtrl', ['$scope', 'MnMedMod', function($scope, MnMedMod) {
+    var calc = new MnMedMod();
     $scope.user = {};
+    $scope.answers = {};
 
     $scope.findMMM = function() {
-      console.log('update running!');
       var input = $scope.user.input.split(' ');
       for (var i = 0; i < input.length; i++) {
         input[i] = +input[i];
       }
-      $http({
-        method: 'POST',
-        url: '/mmm',
-        data: {input: input}
-      })
-      .success(function(data) {
-        console.log(data);
-        $scope.answers = data;
-      })
-      .error(function(data) {
-        console.log(data);
-      });
+      $scope.answers.mean = calc.mean(input);
+      $scope.answers.median = calc.median(input);
+      $scope.answers.mode = calc.mode(input);
     };
   }]);
 };
